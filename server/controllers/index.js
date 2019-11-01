ca99ce636b7b33d96597d32c806ba91dbc638ca9
Sanghoon Kim <lostin185@gmail.com>
2019-11-01 20:42:57 +0900

    :sparkles:191101 2041 충돌해결, 로그인 구현

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,22 +1,45 @@
-const Usermodels = require("../models/usermodel");
+const usermodels = require("../models/usermodel");
 const cropmodels = require("../models/cropmodel");
+const jwt = require("jsonwebtoken");
+
 //jwt 토큰 가져오기
 module.exports = {
   signup: async function(req, res) {
     try {
-      await Usermodels.signup(req.body);
-      res.sendStatus(201);
+      await usermodels.signup(req.body);
+      res.sendStatus(201).redirect(req.originalUrl /*+ login url*/);
       //redirect(홈페이지로...)
     } catch (err) {
       res.sendStatus(500);
     }
   },
   signin: async function(req, res) {
-    let result = await Usermodels.signin();
+    try {
+      let signinData = {
+        email: req.body.email,
+        password: req.body.password
+      };
+
+      let equality = await usermodels.signin(signinData);
+
+      if (equality) {
+        jwt.sign(signinData, "secretkey", { expiresIn: "1h" }, (err, token) => {
+          res.status(200).json({ token });
+        });
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
   },
   signout: function(req, res) {
-    //destory();
-    //로그아웃
+    jwt.verify(req.token, "secretkey", (err, authData) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        let userEmail = authData.email;
+        res.json({ userEmail }); // logout하는 유저의 데이터 (위의 signinData)
+      }
+    });
   },
   reco: async function(req, res) {
     let recoCrops = await cropmodels.reco("fdfs");
