2c5cc07501d40269351a6cd19816f1ab472c0dc0
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2019-11-01 20:01:13 +0900

    :sparkles: 20191101 20:00 crop recommend function

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,41 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
-  let user = sequelize.define(
-    "User",
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      category: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      term: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      difficulty: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      labor: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      }
+  let user = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    {}
-  );
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    term: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    labor: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  });
+  user.associate = function(modles) {
+    user.belongsToMany(modles.crop, { through: "user_crop" });
+  };
   return user;
 };
