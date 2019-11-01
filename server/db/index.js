6643cf5b9f5fdf9a03480c0e2bb87761bfe7cfda
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2019-11-01 15:04:15 +0900

    :sparkles: 20191101 mvc function

--- server/db/index.js
+++
@@ -1,5 +0,0 @@
-const models = require("./models");
-
-models.sequelize.sync().then(() => {
-  console.log("connection established");
-});
--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -1,15 +1,36 @@
-const db = require("../db/models").sequelize;
-const { User } = require("../db/models");
+const user = require("../db/models/index").User;
 
-// const user = require("../db/models/index").user;
+user.sync();
 
-User.sync()
-  .create({
-    username: "김태중",
-    password: "12311",
-    email: "taej12@gmail.com",
-    location: "충청남도"
-  })
-  .then(result => {
-    console.log("result: ", result.id);
-  });
+module.exports = {
+  signup: function(data) {
+    const {
+      email,
+      password,
+      username,
+      location,
+      category,
+      term,
+      difficulty,
+      labor
+    } = data;
+    return new Promise((resolve, reject) => {
+      user
+        .create({
+          username,
+          password,
+          email,
+          location
+        })
+        .then(data => {
+          return resolve("ok");
+        })
+        .catch(err => {
+          return reject(err);
+        });
+    });
+  },
+  signin: function() {
+    return user.findAll().then(data => data[0].dataValues);
+  }
+};
