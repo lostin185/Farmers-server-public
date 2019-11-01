50a77d9b0f4e566ff99bbb08c9d0207ffa8a0034
Sanghoon Kim <lostin185@gmail.com>
2019-11-01 19:52:38 +0900

    :sparkles:191101 1948 apply login/out

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,23 +1,58 @@
 const models = require("../models/usermodel");
-//jwt 토큰 가져오기
+const jwt = require("jsonwebtoken");
+
 module.exports = {
-  signup: async function (req, res) {
+  signup: async function(req, res) {
     try {
       await models.signup(req.body);
-      res.sendStatus(201);
-      //redirect(홈페이지로...)
+      res.redirect(req.originalUrl /*+ login url*/).sendStatus(201);
     } catch (err) {
-      res.sendStatus(500)
+      res.sendStatus(500);
     }
   },
-  signin: async function (req, res) {
-    let result = await models.signin();
+
+  signin: async function(req, res) {
+    try {
+      let signinData = {
+        email: req.body.email,
+        password: req.body.password
+      };
+
+      let equality = await models.signin(signinData);
+
+      if (equality) {
+        jwt.sign(signinData, "secretkey", { expiresIn: "1h" }, (err, token) => {
+          res.status(200).json({ token });
+        });
+      }
+    } catch (err) {
+      res.sendStatus(500);
+    }
+
+    // let signinData = {
+    //   email: req.body.email,
+    //   password: req.body.password
+    // };
+
+    // let equality = await models.signin(signinData);
+
+    // if (equality) {
+    //   jwt.sign(signinData, "secretkey", { expiresIn: "1h" }, (err, token) => {
+    //     res.status(200).json({ token });
+    //   });
+    // } else {
+    //   res.status(404).send("no matching email and password");
+    // }
   },
-  signout: function (req, res) {
-    //destory();
-    //로그아웃
-  },
-  reco: function (req, res) {
-    //추천 작물 가져오기
+
+  signout: function(req, res) {
+    jwt.verify(req.token, "secretkey", (err, authData) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        let userEmail = authData.email;
+        res.json({ userEmail }); // logout하는 유저의 데이터 (위의 signinData)
+      }
+    });
   }
 };
