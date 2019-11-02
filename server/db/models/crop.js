66a3b1724f55da7e6c10e5a9d4d2775aff7b8b84
Sanghoon Kim <lostin185@gmail.com>
2019-11-02 17:04:08 +0900

    :bug:recommend system change - bug: promise

--- server/db/models/crop.js
+++ server/db/models/crop.js
@@ -1,55 +1,49 @@
 module.exports = function(sequelize, DataTypes) {
-  let crop = sequelize.define(
-    "Crop",
-    {
-      cropname: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      category: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      maxtemp: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      mintemp: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      maxph: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      minph: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      term: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      difficulty: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      labor: {
-        type: DataTypes.INTEGER,
-        allowNull: true
-      },
-      method: {
-        type: DataTypes.STRING,
-        allowNull: true
-      }
+  let crop = sequelize.define("Crop", {
+    cropname: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    (crop.associate = function(modles) {
-      crop.belongsToMany(modles.user, { through: "user_crop" });
-    })
-  );
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    maxtemp: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    mintemp: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    maxph: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    minph: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    term: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    difficulty: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    labor: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    method: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
   return crop;
 };
