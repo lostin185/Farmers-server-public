61a71706268325ee99aef82563c59381e88a1b8c
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2019-11-08 16:59:20 +0900

    :sparkles: 20191108 crop 신호등 정보 전달하는 crop/temp 부분 추가

--- server/croproutes.js
+++ server/croproutes.js
@@ -3,5 +3,5 @@ var router = require("express").Router();
 
 //Connect controller methods to their corresponding routes
 router.get("/reco", controller.reco); // email, password, username, location 필수
-
+router.get("/:temp", controller.temp);
 module.exports = router;
--- server/models/cropmodel.js
+++ server/models/cropmodel.js
@@ -3,9 +3,8 @@ const user = require("../db/models/index").User;
 
 crop.sync();
 user.sync();
-
 module.exports = {
-  reco: async function(userEmail) {
+  reco: async function (userEmail) {
     return user
       .findOne({
         where: { email: userEmail }
@@ -25,5 +24,25 @@ module.exports = {
           });
       })
       .catch(err => console.log(err));
+  },
+  level: function (minTemp, maxTemp, temp) {
+    let trafficLight = { level: 0 }
+    return new Promise((resolve, reject) => {
+      if (minTemp - 3 < temp && temp < maxTemp + 3) {
+        trafficLight.level = 2;
+        if (maxTemp < temp && temp < maxTemp) {
+          trafficLight.level = 1;
+        }
+      }
+      else {
+        trafficLight.level = 3;
+      }
+      if (trafficLight.level > 0) {
+        resolve(trafficLight);
+      }
+      else {
+        reject(trafficLight);
+      }
+    })
   }
-};
+}
