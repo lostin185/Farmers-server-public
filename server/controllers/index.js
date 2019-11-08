0416ba1e108149a780f1bfe90a7a5aa92fa0145f
Sanghoon Kim <lostin185@gmail.com>
2019-11-08 10:23:05 +0900

    주석 및 콘솔 정리, jwt를 session으로 대체

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,16 +1,11 @@
 const usermodels = require("../models/usermodel");
 const cropmodels = require("../models/cropmodel");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-//jwt 토큰 가져오기
 module.exports = {
-  signup: async function (req, res) {
-    console.log("req.body", req.body)
+  signup: async function(req, res) {
     try {
       let userSignup = await usermodels.signup(req.body);
-      console.log(
-        "userSignup", userSignup
-      )
       if (userSignup === "success") {
         res.status(201);
         res.send({ success: true });
@@ -22,51 +17,73 @@ module.exports = {
       res.sendStatus(500);
     }
   },
-  signin: async function (req, res) {
+  signin: async function(req, res) {
     try {
       let signinData = {
         email: req.body.email,
         password: req.body.password
       };
       let equality = await usermodels.signin(signinData);
+
+      // session
       if (equality) {
-        jwt.sign({ signinData }, "secretkey", { expiresIn: "30s" }, (err, token) => {
-          return new Promise((resolve, reject) => {
-            if (err) {
-              return reject(err);
-            }
-            else {
-              return resolve(token);
-            }
-          }).then(data => {
-            // setCookie('token', data, 1);
-            console.log("data::::", data)
-            res.cookie("token", data, { httpOnly: true })
-            console.log("make cookie!");
-            res.sendStatus(200);
-          }).catch(err => {
-            if (err) {
-              res.sendStatus(500);
-            }
-          })
-        })
+        var sess = req.session;
+        sess.email = signinData.email;
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(500);
       }
+
+      // // jwt trial
+      // if (equality) {
+      //   jwt.sign({ signinData }, "secretkey", { expiresIn: "1h" }, (err, token) => {
+      //     return new Promise((resolve, reject) => {
+      //       if (err) {
+      //         return reject(err);
+      //       }
+      //       else {
+      //         return resolve(token);
+      //       }
+      //     }).then(data => {
+      //       res.cookie("token", data, { httpOnly: true })
+      //       res.sendStatus(200);
+      //     }).catch(err => {
+      //       if (err) {
+      //         res.sendStatus(500);
+      //       }
+      //     })
+      //   })
+      // }
     } catch (err) {
       res.sendStatus(500);
     }
   },
-  signout: function (req, res) {
-    jwt.verify(req.token, "secretkey", (err, authData) => {
-      if (err) {
-        res.sendStatus(500);
-      } else {
-        let userEmail = authData.email;
-        res.json({ userEmail }); // logout하는 유저의 데이터 (위의 signinData)
-      }
-    });
+  signout: function(req, res) {
+    // session
+    var sess = req.session;
+    if (sess.email) {
+      req.session.destroy(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+    } else {
+      res.sendStatus(500);
+    }
+
+    // // jwt trial
+    // jwt.verify(req.token, "secretkey", (err, authData) => {
+    //   if (err) {
+    //     res.sendStatus(500);
+    //   } else {
+    //     let userEmail = authData.email;
+    //     res.json({ userEmail }); // logout하는 유저의 데이터 (위의 signinData)
+    //   }
+    // });
   },
-  reco: async function (req, res) {
-    //request요청 header에 들어있는 토큰을 가져오기
+  reco: async function(req, res) {
     let recoCrops = await cropmodels.reco("swerty14@naver.com");
     if (recoCrops) {
       console.log("추천작물 성공");
@@ -76,4 +93,4 @@ module.exports = {
       res.sendStatus(500);
     }
   }
-}
+};
--- server/userroutes.js
+++ server/userroutes.js
@@ -1,30 +1,32 @@
 var controller = require("./controllers");
 var router = require("express").Router();
 
-function verifyToken(req, res, next) {
-  // Get auth header value
-  const bearerHeader = req.headers["authorization"];
-  // Check if bearer is undefined
-  if (typeof bearerHeader !== "undefined") {
-    // Split at the space
-    const bearer = bearerHeader.split(" ");
-    // Get token form array
-    const bearerToken = bearer[1];
-    // Set the token
-    req.token = bearerToken;
-    // Next middleware
-    next();
-  } else {
-    // Forbidden
-    res.sendStatus(500);
-  }
-}
+// // jwt trial
+// function verifyToken(req, res, next) {
+//   // Get auth header value
+//   const bearerHeader = req.headers["authorization"];
+//   // Check if bearer is undefined
+//   if (typeof bearerHeader !== "undefined") {
+//     // Split at the space
+//     const bearer = bearerHeader.split(" ");
+//     // Get token form array
+//     const bearerToken = bearer[1];
+//     // Set the token
+//     req.token = bearerToken;
+//     // Next middleware
+//     next();
+//   } else {
+//     // Forbidden
+//     res.sendStatus(500);
+//   }
+// }
 
 //Connect controller methods to their corresponding routes
 router.post("/signup", controller.signup); // email, password, username, location 필수
 
 router.post("/signin", controller.signin);
 
-router.post("/signout", verifyToken, controller.signout);
+// router.post("/signout", verifyToken, controller.signout);
+router.post("/signout", controller.signout);
 
 module.exports = router;
